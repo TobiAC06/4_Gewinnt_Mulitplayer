46e4db29af111350d27f06df904377731f7ad80b
TobiAC06 <radermacher.tobias@t-online.de>
2024-09-18 13:43:31 +0200

    Update Client_4_Gewinnt.java

--- src/Game/Client_4_Gewinnt.java
+++ src/Game/Client_4_Gewinnt.java
@@ -1,12 +1,17 @@
 package Game;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 import javax.swing.*;
 
 public class Client_4_Gewinnt {
 	static private Socket client;
+	static private PrintWriter out;
+	static private BufferedReader in;
 	
 	public static void main(String args[]) {
 		connectToServer();
@@ -28,9 +33,13 @@ public class Client_4_Gewinnt {
 	         int port = Integer.parseInt(portField.getText());
 	         try {
 				client = new Socket(ipField.getText(),port);
+				out = new PrintWriter(client.getOutputStream(),true);
+				in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+				System.out.println(in.readLine());
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
+	         
 	      }
 	      
 	}
