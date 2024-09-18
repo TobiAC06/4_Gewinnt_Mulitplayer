eef13f8c855c41e1afaed24627b1193019c55552
TobiAC06 <radermacher.tobias@t-online.de>
2024-09-18 16:28:15 +0200

    fertig so halb

--- src/Game/Client_4_Gewinnt.java
+++ src/Game/Client_4_Gewinnt.java
@@ -13,12 +13,12 @@ public class Client_4_Gewinnt {
 	static private PrintWriter out;
 	static private BufferedReader in;
 
-	public static void main(String args[]) {
+	public static void main(String args[]) throws Exception {
 		connectToServer();
 	}
 
 	// connects to a Server with a IP and Port given by the User
-	public static void connectToServer() {
+	public static void connectToServer() throws Exception {
 
 		// creates elements for the JOptionPane to create a more or less reasonable
 		// Layout
@@ -33,15 +33,10 @@ public class Client_4_Gewinnt {
 				"Please Enter IP and Port of Server", JOptionPane.OK_CANCEL_OPTION);
 		if (result == JOptionPane.OK_OPTION) {
 			int port = Integer.parseInt(portField.getText());
-			try {
-				client = new Socket(ipField.getText(), port);
-				out = new PrintWriter(client.getOutputStream(), true);
-				in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-				System.out.println(in.readLine());
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-
+			client = new Socket(ipField.getText(), port);
+			out = new PrintWriter(client.getOutputStream(), true);
+			in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+			System.out.println(in.readLine());
 		}
 
 	}
