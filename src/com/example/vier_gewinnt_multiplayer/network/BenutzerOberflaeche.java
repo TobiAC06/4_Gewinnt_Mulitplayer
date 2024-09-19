e00f58cbca20883198249309510347c4dc6e816a
Eco-Gaming <7ecogaming4@gmail.com>
2024-09-19 15:57:28 +0200

    mostly working

---
+++ src/com/example/vier_gewinnt_multiplayer/network/BenutzerOberflaeche.java
@@ -0,0 +1,90 @@
+package com.example.vier_gewinnt_multiplayer.network;
+
+import com.example.vier_gewinnt_multiplayer.Main;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+
+public class BenutzerOberflaeche extends JFrame {
+
+    private static int zuege = 0;
+    private final int roll;
+
+    //contains a String which represents the board: x -> nothing, r -> Player 1, g-> Player 2
+    private String board;
+    JButton[] bt = new JButton[7];
+    JLabel[] lb = new JLabel[42];
+
+
+    //Constructor
+    public BenutzerOberflaeche (String s, int roll) {
+        this.roll = roll;
+        this.board = s;
+        createTheScreen();
+        this.setVisible(true);
+    }
+
+
+    //gets the new board and updates the lables
+    public void updateGame(String s) {
+        zuege++;
+        this.board = s;
+        for (int i = 0; i < board.length(); i++) {
+            if (board.charAt(i) == 'g') {
+                lb[i].setBackground(new Color(0xFFFF00));
+            } else if(board.charAt(i) == 'r') {
+                lb[i].setBackground(new Color(0xFF0000));
+            }
+        }
+    }
+
+    private void actionB(int i) throws IOException, InterruptedException {
+        //do something
+        if (true || zuege % 2 == roll) {
+            System.out.println(i);
+            Main.hostClient.sendMove(i);
+        }
+    }
+
+    //do some UI Stuff
+    private void createTheScreen() {
+        this.setTitle("4 Gewinnt");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setLayout(new GridLayout(7,7));
+        this.setSize(700, 700);
+        this.getContentPane().setBackground(Color.white);
+
+
+        for (int i = 0; i < 7; i++) {
+            JButton b = new JButton();
+            int number = i;
+            b.addActionListener(e -> {
+                try {
+                    actionB(number);
+                } catch (IOException | InterruptedException ex) {
+                    throw new RuntimeException(ex);
+                }
+            });
+            b.setBackground(new Color(0xFFFFFF));
+            b.setText("↓");
+            b.setFont(new Font("", Font.BOLD, 50));
+            bt[i] = b;
+            this.add(bt[i]);
+        }
+
+        for (int i = 0; i < board.length(); i++) {
+            JLabel f = new JLabel();
+            f.setOpaque(true);
+            if (board.charAt(i) == 'g') {
+                f.setBackground(new Color(0xFFFF00));
+            } else if(board.charAt(i) == 'r') {
+                f.setBackground(new Color(0xFF0000));
+            }
+            f.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+            lb[i] = f;
+            this.add(lb[i]);
+        }
+    }
+}
---
+++ src/com/example/vier_gewinnt_multiplayer/network/Server.java
@@ -0,0 +1,155 @@
+package com.example.vier_gewinnt_multiplayer.network;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server implements Runnable {
+
+    int port;
+
+    public ServerSocket socket;
+
+    private Socket hostClient;
+    private PrintWriter host_out;
+    private BufferedReader host_in;
+
+    private Socket client;
+    private PrintWriter client_out;
+    private BufferedReader client_in;
+
+    private char[][] board = { // board[spalte][zeile]
+            { 'x', 'x', 'x', 'x', 'x', 'x' ,'x'},
+            { 'x', 'x', 'x', 'x', 'x', 'x' ,'x'},
+            { 'x', 'x', 'x', 'x', 'x', 'x' ,'x'},
+            { 'x', 'x', 'x', 'x', 'x', 'x' ,'x'},
+            { 'x', 'x', 'x', 'x', 'x', 'x' ,'x'},
+            { 'x', 'x', 'x', 'x', 'x', 'x' ,'x'}
+    };
+
+    public Server(int port) throws Exception {
+        this.port = port;
+    }
+
+    @Override
+    public void run() {
+        try {
+            startServer(port);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void startServer(int port) throws Exception {
+        // initialize a server on the given port
+        socket = new ServerSocket(port);
+
+        // accept hostClient
+        System.out.print("Awaiting hostClient connection...");
+        hostClient = socket.accept();
+        host_out = new PrintWriter(hostClient.getOutputStream(), true);
+        host_in = new BufferedReader(new InputStreamReader(hostClient.getInputStream()));
+        System.out.println("hostClient connected!");
+
+        // accept client
+        if (true) {
+            System.out.print("Awaiting client connection...");
+            client = socket.accept();
+            client_out = new PrintWriter(client.getOutputStream(), true);
+            client_in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            System.out.println("client connected!");
+        }
+
+        sendeSpielfeld();
+        startGame();
+    }
+
+    private void startGame() throws Exception {
+        boolean hostWins = false;
+        boolean clientWins = false;
+        boolean hostsTurn = true;
+        while (hostWins == clientWins) {
+            if (hostsTurn) {
+                int pSpalte = Integer.parseInt(host_in.readLine());
+                System.out.println("host move");
+                if (spalteIstFrei(pSpalte)) {
+                    for (int i = board.length-1; i >= 0; i--) {
+                        if (board[i][pSpalte] == 'x') {
+                            board[i][pSpalte] = 'r';
+                            hostWins = prüfeSieger('r', pSpalte);
+                            sendeSpielfeld();
+                            hostsTurn = false;
+                            break;
+                        }
+                    }
+                }
+            } else {
+                int pSpalte = Integer.parseInt(client_in.readLine());
+                System.out.println("client move");
+                if (spalteIstFrei(pSpalte)) {
+                    for (int i = board.length-1; i >= 0; i--) {
+                        if (board[i][pSpalte] == 'x') {
+                            board[i][pSpalte] = 'g';
+                            clientWins = prüfeSieger('g', pSpalte);
+                            sendeSpielfeld();
+                            hostsTurn = true;
+                            break;
+                        }
+                    }
+                }
+
+            }
+        }
+        System.out.println("Game Over. The winner is: " + (hostWins ? "host" : "client") + "!");
+    }
+
+    private boolean spalteIstFrei(int spalte) {
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][spalte] == 'x') {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean prüfeSieger(char spieler, int spalte) {
+        String siegerString = "" + spieler + spieler + spieler + spieler;
+        int zeile = 0;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][spalte] != 'x') {
+                zeile = i;
+                break;
+            }
+        }
+        String horizontal = "";
+        String vertikal = "";
+        String diagonalEins = "";
+        String diagonalZwei = "";
+        for (int i = -6; i <= 6; i++) {
+            if (zeile + i >= 0 && zeile + i <= 5) horizontal += board[zeile + i][spalte];
+            if (spalte + i >= 0 && spalte + i <= 6) vertikal += board[zeile][spalte + i];
+            if (spalte + i >= 0 && spalte + i <= 6 && zeile + i >= 0 && zeile + i <= 5) diagonalEins += board[zeile + i][spalte + i];
+            if (spalte - i >= 0 && spalte - i <= 6 && zeile + i >= 0 && zeile + i <= 5)diagonalZwei += board[zeile + i][spalte - i];
+        }
+        if (horizontal.indexOf(siegerString) >= 0 || vertikal.indexOf(siegerString) >= 0
+                || diagonalEins.indexOf(siegerString) >= 0 || diagonalZwei.indexOf(siegerString) >= 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void sendeSpielfeld() {
+        String sending = "";
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                sending += board[i][j];
+            }
+        }
+        host_out.println(sending);
+        if (true) client_out.println(sending);
+    }
+
+}
